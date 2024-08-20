a6140c7bc1498a0b3072fad263ef08441596e071
korniyenko88 <165329483+korniyenko88@users.noreply.github.com>
2024-08-20 12:04:39 +0000

    Deploying to gh-pages from @ korniyenko88/goit-js-hw-11@b536cb47b8bde11055c1fe5c7575efb851b9fb59 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,22 @@
-import{i}from"./assets/vendor-ad859c2f.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const n=document.querySelector(".js-search-form"),l=document.querySelector(".js-gallery"),u=o=>`
-    <li class="gallery-card">
-    <img class="galler-card" src="${o.webformatURL}" alt="${o.tags}" />
-    </li>
-    `,d=o=>{o.preventDefault();const s=n.elements.user_query.value;fetch(`https://pixabay.com/api/?key=45483609-d3ae590ff20ddbd4abc31de80&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{console.log(t);const a=t.hits.map(e=>u(e)).join("");l.innerHTML=a}).catch(t=>{console.log(t),i.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})})};n.addEventListener("submit",d);
+import"./assets/vendor-b20c67ea.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&a(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const c=r=>`
+  <li class="gallery-card">
+  <a class="gallery-link" href="${r.largeImageURL}">
+    <img class="gallery-card" src="${r.webformatURL}" alt="${r.tags}" />
+    <div class="card-info">
+      <p class="info-item">
+        <b>Likes</b> ${r.likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b> ${r.views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b> ${r.comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b> ${r.downloads}
+      </p>
+    </div>
+  </a>
+</li>
+`,i=document.querySelector(".js-search-form"),n=document.querySelector(".js-gallery"),u=r=>{r.preventDefault();const s=i.elements.user_query.value;fetch(`https://pixabay.com/api/?key=45483609-d3ae590ff20ddbd4abc31de80&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()}).then(o=>{console.log(o);const a=o.hits.map(e=>c(e)).join("");n.innerHTML=a,new SimpleLightbox(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250})}).catch(o=>{console.log(o),iziToast.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})})};i.addEventListener("submit",u);
 //# sourceMappingURL=commonHelpers.js.map
