5f1caab64ac9517aa5bbc4a30ebe8712f2e49222
korniyenko88 <165329483+korniyenko88@users.noreply.github.com>
2024-08-20 13:29:56 +0000

    Deploying to gh-pages from @ korniyenko88/goit-js-hw-11@4d1ac5b2860c4291a8d9f9c7d8e0fadb5fc3df6d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,22 +1,22 @@
-import"./assets/vendor-b20c67ea.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&a(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const c=r=>`
+import{S as n,i as c}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&t(l)}).observe(document,{childList:!0,subtree:!0});function a(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=a(e);fetch(e.href,r)}})();const u=o=>`
   <li class="gallery-card">
-  <a class="gallery-link" href="${r.largeImageURL}">
-    <img class="gallery-card" src="${r.webformatURL}" alt="${r.tags}" />
+  <a class="gallery-link" href="${o.largeImageURL}">
+    <img class="gallery-card" src="${o.webformatURL}" alt="${o.tags}" />
     <div class="card-info">
       <p class="info-item">
-        <b>Likes</b> ${r.likes}
+        <b>Likes</b> ${o.likes}
       </p>
       <p class="info-item">
-        <b>Views</b> ${r.views}
+        <b>Views</b> ${o.views}
       </p>
       <p class="info-item">
-        <b>Comments</b> ${r.comments}
+        <b>Comments</b> ${o.comments}
       </p>
       <p class="info-item">
-        <b>Downloads</b> ${r.downloads}
+        <b>Downloads</b> ${o.downloads}
       </p>
     </div>
   </a>
 </li>
-`,i=document.querySelector(".js-search-form"),n=document.querySelector(".js-gallery"),u=r=>{r.preventDefault();const s=i.elements.user_query.value;fetch(`https://pixabay.com/api/?key=45483609-d3ae590ff20ddbd4abc31de80&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()}).then(o=>{console.log(o);const a=o.hits.map(e=>c(e)).join("");n.innerHTML=a,new SimpleLightbox(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250})}).catch(o=>{console.log(o),iziToast.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})})};i.addEventListener("submit",u);
+`,i=document.querySelector(".js-search-form"),d=document.querySelector(".js-gallery"),p=o=>{o.preventDefault();const s=i.elements.user_query.value,a=document.querySelector(".loader");a.style.display="inline-block",fetch(`https://pixabay.com/api/?key=45483609-d3ae590ff20ddbd4abc31de80&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{if(t.hits.length===0)throw new Error("No images found.");console.log(t);const e=t.hits.map(l=>u(l)).join("");d.innerHTML=e,new n(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}).refresh(),i.reset()}).catch(t=>{console.log(t),c.error({title:"Error",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"})}).finally(()=>{a.style.display="none"})};i.addEventListener("submit",p);
 //# sourceMappingURL=commonHelpers.js.map
